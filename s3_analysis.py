31400b0ff886861233afeb4469c3645ed377836f
aclaypool <adam.claypool@tradingtechnologies.com>
2017-10-18 20:55:58 -0500

    bug_fixes

--- s3_analysis.py
+++ s3_analysis.py
@@ -10,6 +10,9 @@ from my_thread import ThreadPool
 
 class report(object):
     def __init__(self, logger, creds_file='~/.aws/credentials', exclude_b=None, include_b=None, file_count=None, sort_order="newest", workers=10, acct_profile=None):
+        self.logger = logger
+        self.account_profiles = []
+        self.creds_file = creds_file
         self.acct_profile  = acct_profile
         self.include = include_b
         self.exclude = exclude_b
@@ -18,21 +21,20 @@ class report(object):
         self.bucket_list = []
 
         if self.acct_profile:
-            logger.info("Verifying that {} profile can be found in {}".format(self.acct_profile, creds_file))
-            self.verify_account_profile(self.acct_profile, creds_file)
-            self.get_bucket_list([self.acct_profile])
+            self.logger.info("Verifying that {} profile can be found in {}".format(self.acct_profile, creds_file))
+            self.verify_account_profile()
+            self.get_bucket_list()
         else:
-            logger.info("Finding all account profiles in {}.".format(creds_file))
-            self.get_bucket_list(self.get_account_profiles(creds_file))
+            self.logger.info("Finding all account profiles in {}.".format(creds_file))
+            self.get_bucket_list(self.get_account_profiles())
 
-    def get_account_profiles(self, creds_file):
-        account_profiles = []
-        if '~/' in creds_file:
-            full_path = os.path.expanduser(creds_file)
+    def get_account_profiles(self):
+        if '~/' in self.creds_file:
+            full_path = os.path.expanduser(self.creds_file)
         else:
-            full_path = os.path.abspath(creds_file)
+            full_path = os.path.abspath(self.creds_file)
 
-        logger.info("Full path for credentials file is {}".format(full_path))
+        self.logger.info("Full path for credentials file is {}".format(full_path))
 
         if os.path.exists(full_path):
             os.environ['AWS_SHARED_CREDENTIALS_FILE'] = full_path
@@ -40,21 +42,22 @@ class report(object):
                 contents = f.read()
             for line in contents.split('\n'):
                 if line.strip().startswith('[') and line.strip().endswith(']'):
-                    tmp_profile = line.replace('[', '').replace(']', '')
-                    logger.info("From {} extracted account profile named {}".format(line, tmp_profile))
-                    account_profiles.append(tmp_profile)
+                    tmp = line.replace('[', '').replace(']', '').strip()
+                    logger.info("From {} extracted account profile named {}".format(line, tmp))
+                    self.account_profiles.append(tmp)
         else:
             sys.exit("Unable to locate the provided creds file. Please verify that it exists and that you have permissions to read.")
 
-        return account_profiles
+        return self.account_profiles
 
-    def get_buckets(self, name, creation, session, sort_order, logger):
-        self.bucket_list.append(Bucket(name, creation, session, sort_order, logger))
+    def get_buckets(self, name, creation, session, sort_order):
+        self.bucket_list.append(Bucket(name, creation, session, sort_order, self.logger))
 
     def get_bucket_list(self, account_profiles):
-        buckets = []
+
         pool = ThreadPool(self.workers)
         for profile in account_profiles:
+            buckets = []
             session = boto3.Session(profile_name=profile)
             s3_client = session.client('s3')
             bucket_list = s3_client.list_buckets()['Buckets']
@@ -69,42 +72,42 @@ class report(object):
 
             if self.include != None:
                 for i in self.include:
-                    logger.info("Including buckets named {}".format(i))
+                    self.logger.info("Including any buckets matching {} from account {}.".format(i,profile))
                     for bd in bucket_list:
                         if i in bd['Name']:
-                            logger.info("Found and including bucket {}".format(bd['Name']))
+                            self.logger.info("Found and including bucket {}".format(bd['Name']))
                             buckets.append({
                                     'Name': bd['Name'],
                                     'CreationDate': bd['CreationDate']
                                     },)
-                    for bucket in buckets:
-                        pool.add_task(self.get_buckets,bucket['Name'],bucket['CreationDate'],session,self.sort_order,logger)
+                for bucket in buckets:
+                    pool.add_task(self.get_buckets(bucket['Name'],bucket['CreationDate'],session,self.sort_order))
             else:
                 for bd in bucket_list:
-                    logger.info("Including bucket {}".format(bd['Name']))
-                    pool.add_task(self.get_buckets,bd['Name'], bd['CreationDate'], session, self.sort_order,logger)
-            pool.wait_completion()
+                    self.logger.info("Including bucket {}".format(bd['Name']))
+                    pool.add_task(self.get_buckets(bd['Name'], bd['CreationDate'], session, self.sort_order))
+        pool.wait_completion()
 
-    def verify_account_profile(self, acct_profile, creds_file):
+    def verify_account_profile(self):
         found_match = False
-        if '~/' in creds_file:
-            full_path = os.path.expanduser(creds_file)
+        if '~/' in self.creds_file:
+            full_path = os.path.expanduser(self.creds_file)
         else:
-            full_path = os.path.abspath(creds_file)
+            full_path = os.path.abspath(self.creds_file)
 
-        logger.info("Full path for credentials file is {}".format(full_path))
+        self.logger.info("Full path for credentials file is {}".format(full_path))
 
         if os.path.exists(full_path):
             os.environ['AWS_SHARED_CREDENTIALS_FILE'] = full_path
             with open(full_path, "r") as f:
                 contents = f.read()
             for line in contents.split('\n'):
-                if acct_profile in line:
+                if self.acct_profile in line:
                     found_match = True
         if found_match:
             return True
         else:
-            sys.exit("Unable to find profile {} in {}".format(self.acct_profile, creds_file))
+            sys.exit("Unable to find profile {} in {}".format(self.acct_profile, self.creds_file))
 
 def analysis_log(log_file):
     logger = logging.getLogger('s3_analysis')
