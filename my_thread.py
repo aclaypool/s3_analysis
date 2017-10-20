b845e75096c74ec80c19f316190bcb16d15de832
aclaypool <adam.claypool@tradingtechnologies.com>
2017-10-20 01:17:16 -0500

    final commit

--- my_thread.py
+++ my_thread.py
@@ -1,6 +1,7 @@
 from Queue import Queue
 from threading import Thread
 
+# Worker for multithreading all though adding subprocess prevents
 class Worker(Thread):
     def __init__(self, tasks):
         Thread.__init__(self)
@@ -18,6 +19,7 @@ class Worker(Thread):
                     print "Unable to run worker: {} Function: {} Args: {}".format(e, func, args)
             self.tasks.task_done()
 
+# Thread class for multithreading 
 class ThreadPool(object):
     def __init__(self, num_threads):
         self.tasks = Queue(num_threads)
